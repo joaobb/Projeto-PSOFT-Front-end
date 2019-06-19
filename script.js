ea93e03777e2149647c432baf677ea7366136326
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2019-06-19 02:01:05 -0300

    Working on something :D

---
+++ script.js
@@ -0,0 +1,87 @@
+function emailValidator(email) {
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return  regex.test(email);
+}
+
+passwdValidator = passwd => passwd.length > 7;
+
+validEmail = (email, element) => document.getElementById("emailVality" + element).innerText = emailValidator(email) ? "Valid Email" : "Invalid Email";
+validPasswd = (passwd, element) => document.getElementById("passwdVality" + element).innerText = passwdValidator(passwd) ? "Valid Password" : "Invalid Password";
+
+function dataSignUp() {
+  const $firstName = document.forms["signUp"]["firstName"].value;
+  const $lastName = document.forms["signUp"]["lastName"].value;
+  const $email = document.forms["signUp"]["email"].value;
+  const $password = document.forms["signUp"]["password"].value;
+  if ($firstName == "" || $lastName == "" || $email == "" || $password == "" || !emailValidator($email) || !passwdValidator($password)) {
+    alert("Invalid input")
+    throw new Error("Invalid input")
+  }
+   const req = {
+    firstName: $firstName,
+    lastName: $lastName,
+    email: $email,
+    password: $password
+  }
+  return JSON.stringify(req);
+}
+
+function dataSignIn() {
+  const $email = document.forms["signIn"]["email"].value;
+  const $password = document.forms["signIn"]["password"].value;
+  if ($email == "" || $password == "" || !emailValidator($email) || !passwdValidator($password)) {
+    alert("Invalid input")
+    throw new Error("Invalid input")
+  }
+   const req = {
+    email: $email,
+    password: $password
+  }
+  return JSON.stringify(req);
+}
+
+function signin() {
+  fetch("https://ucdb-plataform1.herokuapp.com/api/v1/auth/login",{
+    method : "POST",
+    headers :  {'Content-Type': 'application/json; charset=utf-8'},
+    body : dataSignIn()
+  }).then(response => {
+    if(!response.ok) throw new Error("Registration failed");
+    return response.text();
+  }).then(data => alert(data));
+  console.log("Logado");
+}
+
+function signup() {
+  fetch("https://ucdb-plataform1.herokuapp.com/api/v1/users/", {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json; charset=utf-8'},
+    body: dataSignUp()
+    })
+  .then(response => {
+    if(!response.ok) throw new Error("Registration failed");
+    return response.text();
+  })
+  .then(data => alert("User was successfully registered"));
+  console.log("Registrado");
+
+  return false;
+}
+
+var modSignIn= document.getElementById("signInMod")
+var modSignUp = document.getElementById("signUpMod")
+
+document.getElementById("loginBt").onclick = () => {
+  modSignIn.style.display="block";
+}
+
+document.getElementById("registerBt").onclick = () => {
+  modSignUp.style.display="block";
+}
+
+window.onclick = (event) => {
+  if (event.target == modSignIn || event.target == modSignUp) {
+    modSignIn.style.display = "none"
+    modSignUp.style.display = "none"
+  }
+}
