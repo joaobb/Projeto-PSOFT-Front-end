02741ebbc9872aa91c4a2a45958ce5ae72d85777
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2019-06-23 01:57:21 -0300

    Adição de buffer para input de nome da disciplina

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function validEmail(email) {
-    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return  regex.test(email);
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 }
 
 validPasswd = passwd => passwd.length > 7;
@@ -11,11 +11,11 @@ function dataSignUp() {
   const $lastName = document.forms["signUp"]["lastName"].value;
   const $email = document.forms["signUp"]["email"].value;
   const $password = document.forms["signUp"]["password"].value;
-  if ($firstName == "" || $lastName == "" || $email == "" || $password == "" || !emailValidator($email) || !passwdValidator($password)) {
+  if ($firstName == "" || $lastName == "" || $email == "" || $password == "" || !validEmail($email) || !validPasswd($password)) {
     alert("Invalid input")
     throw new Error("Invalid input")
   }
-   const req = {
+  const req = {
     firstName: $firstName,
     lastName: $lastName,
     email: $email,
@@ -27,11 +27,11 @@ function dataSignUp() {
 function dataSignIn() {
   const $email = document.forms["signIn"]["email"].value;
   const $password = document.forms["signIn"]["password"].value;
-  if ($email == "" || $password == "" || !emailValidator($email) || !passwdValidator($password)) {
+  if ($email == "" || $password == "" || !validEmail($email) || !validPasswd($password)) {
     alert("Invalid input")
     throw new Error("Invalid input")
   }
-   const req = {
+  const req = {
     email: $email,
     password: $password
   }
@@ -43,108 +43,131 @@ function dataSignIn() {
 // * SIGNIN AND SIGNUP FETCHS BEGIN * \\
 
 function signin() {
-  fetch("https://ucdb-plataform1.herokuapp.com/api/v1/auth/login",{
-    method : "POST",
-    headers :  {'Content-Type': 'application/json; charset=utf-8'},
-    body : dataSignIn()
+  fetch("https://ucdb-plataform1.herokuapp.com/api/v1/auth/login", {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    body: dataSignIn()
   }).then(response => {
-    if(!response.ok) throw new Error("Registration failed");
+    console.log(response)
+    if (!response.ok) throw new Error("Registration failed");
     return response.text();
-  }).then(data => alert(data));
+  }).then(data => {
+    console.log(data)
+    // localStorage.setItem("tk", data) 
+    // alert(localStorage.getItem("tk"))
+  });
   console.log("Logado");
 }
 
 function signup() {
   fetch("https://ucdb-plataform1.herokuapp.com/api/v1/users/", {
     method: 'POST',
-    headers: {'Content-Type': 'application/json; charset=utf-8'},
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
     body: dataSignUp()
-    })
-  .then(response => {
-    if(!response.ok) throw new Error("Registration failed");
-    return response.text();
   })
-  .then(data => alert("User was successfully registered"));
+    .then(response => {
+      if (!response.ok) throw new Error("Registration failed");
+      return response.text();
+    })
+    .then(data => alert("User was successfully registered"));
   console.log("Registrado");
 
   return false;
 }
 
+
+
+
 // * SIGNIN AND SIGNUP FETCHS END * \\
 
-const modSignIn= document.getElementById("signInMod");
-const modSignUp = document.getElementById("signUpMod");
-const $discSearchBar = document.getElementById("discSearchBar");
-const $discBt = document.getElementsByClassName("discBt");
+const modSignIn = document.getElementById("signInMod");                //SignIn button
+const modSignUp = document.getElementById("signUpMod");                //SignUp button
+const $discSearchBar = document.getElementById("discSearchBar");       //Discipline search bar
+const $discBt = document.getElementsByClassName("discBt");             //Discipline button
+var bufferTime = null;                                                 //Time to buffer user input on discipline search bar
 
-$discBt.onclick = () => {
-  console.log($discBt.id)
+document.getElementById("loginBt").onclick = () => {                   //Opens SignIn modal
+  modSignIn.style.display = "block";
 }
 
-document.getElementById("loginBt").onclick = () => {
-  modSignIn.style.display="block";
+document.getElementById("registerBt").onclick = () => {                //Opens SignUp modal
+  modSignUp.style.display = "block";
 }
 
-document.getElementById("registerBt").onclick = () => {
-  modSignUp.style.display="block";
-}
-
-window.onclick = (event) => {
+window.onclick = (event) => {                                          //Closes both modals if any of those are open
   if (event.target == modSignIn || event.target == modSignUp) {
     modSignIn.style.display = "none"
     modSignUp.style.display = "none"
   }
 }
 
-$discSearchBar.onkeyup = async function ()  {
-  console.log($discSearchBar.value)
+$discSearchBar.onkeyup = async function () {                           //Receaves user input on the discipline search bar
   if ($discSearchBar.value == "") {
     killAllChildren()
     return;
-  } 
-  await controler()
+  }
+  
+  if (bufferTime != null) clearTimeout(bufferTime);                           //Restart the buffer if it is the first time being called
+  bufferTime = setTimeout(await disciplineBtControler, 300);                  //Starts the buffer countdown, if the uses don't write anything new in 300ms, 
+                                                                              //it will run the button creator function
 }
 
-capitalize = function(string) {
+$discBt.onclick = async function()  {                                  //Opens a modal with the discipline information
+  console.log($discBt.id)
+  // await perfilFetcher()
+}
+
+var disciplineBtControler = async function () {                        //Controls the discipline button creation
+
+  const disciplineList = await disciplineFethcer()
+
+  killAllChildren(); //:D
+
+  disciplineList.forEach(discipline => {
+    discButtonCreator(discipline["id"], discipline["nome"]);
+  });
+}
+
+capitalize = function (string) {                                       //Capitalizes the arg string
   string = string.toLowerCase()
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-// * DISCIPLINES SEARCH BAR RESULTS BEGIN * \\
-
-function discButtonCreator (discId, discNome) { // Creates buttons based on the input
-  let button = document.createElement("button");
-  button.id = discId;
-  button.innerText = (discId + " - " + capitalize(discNome));
-  button.className = "discBt";
-  
-  document.querySelector(".disciplinas-container").appendChild(button);
-}
-
-function killAllChildren() {  //Cleans disciplinas container
+function killAllChildren() {                                           //Cleans disciplinas container
   let $discContainer = document.querySelector(".disciplinas-container");
   while ($discContainer.firstChild) {
-      $discContainer.removeChild($discContainer.firstChild);
+    $discContainer.removeChild($discContainer.firstChild);
   }
 }
 
-var controler = async function () {
+// * DISCIPLINES SEARCH BAR RESULTS BEGIN * \\
 
-  var discList = await discFethcer()
+function discButtonCreator(discId, discNome) {                         // Creates buttons based on the input and set its attributes
+  let button = document.createElement("button");
+  button.id = discId;
+  button.innerText = (discId + " - " + capitalize(discNome));
+  button.onclick = () => console.log(discId);
+  button.className = "discBt";
 
-  killAllChildren(); //:D
-
-  discList.forEach(discipline => {
-      discButtonCreator(discipline["id"], discipline["nome"]);
-  });
+  document.querySelector(".disciplinas-container").appendChild(button);
 }
 
-discFethcer = async function () {
+disciplineFethcer = async function () {                                //Fetches a discipline using a substring on the back-end using 
   const requestUrl = "https://ucdb-plataform1.herokuapp.com/api/v1/disciplina/findSubjects/?substring=" + $discSearchBar.value.replace(/ /g, "%20");
   let fetcher = await fetch(requestUrl)
-  if(!fetcher.ok) throw new Error("Fetch failed");
+  if (!fetcher.ok) throw new Error("Discipline fetch failed");
   let discJson = await fetcher.json();
+
   return discJson;
 }
 
+perfilFetcher = async function (id) {                                  //Fetches for a perfil of a discipline using its id on the back-end
+  const requestUrl = "https://ucdb-plataform1.herokuapp.com/api/v1/disciplina/perfis/?id=" + id;
+
+  let fetcher = await fetch(requestUrl)
+  if (!fetcher.ok) throw new Error("Discipline perfil fetch failed");
+  let perfilData = await fetcher.json();
+
+  console.log(perfilData);
+}
 // * DISCIPLINES SEARCH BAR RESULTS END * \\
